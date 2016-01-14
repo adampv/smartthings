439d9ea20038772f146156828a4b45b56b37dacb
adampv <adam@adamvine.co.uk>
2016-01-14 22:02:01 +0000

    Update SecureWallController.groovy

--- SecureWallController.groovy
+++ SecureWallController.groovy
@@ -1,5 +1,6 @@
 /**
  *  Copyright 2015 AdamV
+ *  Contoller Configuration code thanks to Stuart Buchanan
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
